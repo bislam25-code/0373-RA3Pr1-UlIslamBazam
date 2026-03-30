438b6405c72df8e0a74ea9a622e83c9819880c58
Bazam <bislam25@ilb.cat>
2026-03-30 21:08:33 +0200

    functions

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ let cosTaula = document.getElementById("cosTaula");
 
 let btnAsc = document.getElementById("ordenarAsc");
 let btnDesc = document.getElementById("ordenarDesc");
+
+formulari.addEventListener("submit", validarFormulari);
+btnAsc.addEventListener("click", ordenarAsc);
+btnDesc.addEventListener("click", ordenarDesc);
+
+function validarFormulari(e){
+    e.preventDefault();
+
+    let nom = document.getElementById("nom").value;
+    let examen = parseFloat(document.getElementById("examen").value);
+    let practiques = parseFloat(document.getElementById("practiques").value);
+    let actitud = parseFloat(document.getElementById("actitud").value);
+    
+}
