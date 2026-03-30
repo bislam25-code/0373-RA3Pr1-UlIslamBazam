00953c18deb5343cdabf13d8b23617203f471e7c
Bazam <bislam25@ilb.cat>
2026-03-30 21:04:59 +0200

    elements

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let alumnes = [];
+
+
+let formulari = document.getElementById("formulariAlumne");
+
+let missatge = document.getElementById("missatge");
+
+let cosTaula = document.getElementById("cosTaula");
+
+
+let btnAsc = document.getElementById("ordenarAsc");
+let btnDesc = document.getElementById("ordenarDesc");
